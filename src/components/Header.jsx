73afb1a7bf58838eef8ffa9ab38a07b5b163496d
Chamod Malindu <chamod10malindu@gmail.com>
2025-08-29 00:47:45 +0530

    Add Header component and ClientHomePage with routing 🧩

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return(
+    <div className="w-full h-[100px] bg-blue-600 flex justify-center items-center text-white text-2xl">
+      <Link to="/">Home</Link>
+      <Link to="/products" className="mx-5">Products</Link>
+      <Link to="/reviews">Review</Link>
+      <Link to="/aboutUS" className="mx-5">AboutUs</Link>
+      <Link to="/contactUS">ContactUs</Link>
+    </div>
+  )
+}
---
+++ src/pages/client/clientHomePage.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "../../components/Header";
+
+export default function ClientHomePage() {
+  return (
+    <div className="w-full h-screen">
+      <Header />
+      <Routes path="/">
+        <Route path="/" element={<h1 className="text-center mt-10 text-3xl font-bold">Welcome to the Home Page</h1>} />
+        <Route path="/products" element={<h1 className="text-center mt-10 text-3xl font-bold">Products Page</h1>} />
+        <Route path="/reviews" element={<h1 className="text-center mt-10 text-3xl font-bold">Reviews Page</h1>} />
+        <Route path="/aboutUS" element={<h1 className="text-center mt-10 text-3xl font-bold">About Us Page</h1>} />
+        <Route path="/contactUS" element={<h1 className="text-center mt-10 text-3xl font-bold">Contact Us Page</h1>} />
+        <Route path="/*" element={<h1 className="text-center mt-10 text-3xl font-bold">404 - Page Not Found</h1>} />
+      </Routes>
+    </div>
+  )
+
+}
