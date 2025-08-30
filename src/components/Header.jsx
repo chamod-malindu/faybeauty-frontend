eb58ec504cd3aeeb675dba02128e9f7507506f63
Chamod Malindu <chamod10malindu@gmail.com>
2025-08-30 09:39:56 +0530

    Add margin-bottom to Header component 🪛

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return(
-    <div className="w-full h-[100px] bg-blue-600 flex justify-center items-center text-white text-2xl">
+    <div className="w-full h-[100px] bg-blue-600 flex justify-center items-center text-white text-2xl mb-[20px]">
       <Link to="/">Home</Link>
       <Link to="/products" className="mx-5">Products</Link>
       <Link to="/reviews">Review</Link>
