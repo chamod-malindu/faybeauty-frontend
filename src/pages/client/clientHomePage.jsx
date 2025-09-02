485ed46ba0eee02a6b792c7852cbf5cab4954538
Chamod Malindu <chamod10malindu@gmail.com>
2025-09-03 00:51:27 +0530

    Add Product Overview Page and update route 🧩

--- src/pages/client/clientHomePage.jsx
+++ src/pages/client/clientHomePage.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "../../components/Header";
 import ProductsPage from "./productsPage";
+import ProductOverviewPage from "./productOverviewPage";
 
 export default function ClientHomePage() {
   return (
@@ -12,6 +13,7 @@ export default function ClientHomePage() {
         <Route path="/reviews" element={<h1 className="text-center mt-10 text-3xl font-bold">Reviews Page</h1>} />
         <Route path="/aboutUS" element={<h1 className="text-center mt-10 text-3xl font-bold">About Us Page</h1>} />
         <Route path="/contactUS" element={<h1 className="text-center mt-10 text-3xl font-bold">Contact Us Page</h1>} />
+        <Route path="/overview" element={<ProductOverviewPage />} />
         <Route path="/*" element={<h1 className="text-center mt-10 text-3xl font-bold">404 - Page Not Found</h1>} />
       </Routes>
     </div>
---
+++ src/pages/client/productOverviewPage.jsx
@@ -0,0 +1,7 @@
+export default function ProductOverviewPage() {
+  return(
+    <div className="w-full h-screen">
+      <h1 className="text-center mt-10 text-3xl font-bold">Product Overview Page</h1>
+    </div>
+  )
+}
