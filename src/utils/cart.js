a9b2179b0ef031987b926ddf30c1f9dbab5b0736
Chamod Malindu <chamod10malindu@gmail.com>
2025-09-09 21:01:35 +0530

    Add cart utility functions for managing shopping cart in local storage 🧩

---
+++ src/utils/cart.js
@@ -0,0 +1,48 @@
+export function getCart(){
+  let cartInString = localStorage.getItem("cart");
+
+  if(cartInString == null){
+    cartInString = "[]";
+    localStorage.setItem("cart", cartInString);
+  }
+
+  const cart = JSON.parse(cartInString);
+  return cart;
+}
+
+export function addToCart(product, quantity){
+  const cart = getCart();
+
+  const existingProductIndex = cart.findIndex(
+    (item) => {
+      return item.productId === product.productId;
+    }
+  )
+
+  if(existingProductIndex == -1){
+    cart.push(
+      {
+        productId: product.productId,
+        quantity: quantity,
+        price: product.price,
+        name: product.name,
+        image: product.images[0]
+      }
+    )
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }else{
+    const newQuantity = cart[existingProductIndex].quantity += quantity;
+    if(newQuantity <= 0){
+      const newCart = cart.filter((item, index) => {
+        return index !== existingProductIndex;
+      })
+      localStorage.setItem("cart", JSON.stringify(newCart));
+
+    }else{
+      cart[existingProductIndex].quantity = newQuantity;
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }
+  }
+
+  
