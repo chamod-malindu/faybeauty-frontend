d8e7ed218ce371420cdd4c164336ab01e034cfb1
Chamod Malindu <chamod10malindu@gmail.com>
2025-09-09 21:28:53 +0530

    Add CartPage route to ClientHomePage 🧩

--- src/pages/client/clientHomePage.jsx
+++ src/pages/client/clientHomePage.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "../../components/Header";
 import ProductsPage from "./productsPage";
 import ProductOverviewPage from "./productOverviewPage";
+import CartPage from "./cartPage";
 
 export default function ClientHomePage() {
   return (
@@ -13,6 +14,7 @@ export default function ClientHomePage() {
         <Route path="/reviews" element={<h1 className="text-center mt-10 text-3xl font-bold">Reviews Page</h1>} />
         <Route path="/aboutUS" element={<h1 className="text-center mt-10 text-3xl font-bold">About Us Page</h1>} />
         <Route path="/contactUS" element={<h1 className="text-center mt-10 text-3xl font-bold">Contact Us Page</h1>} />
+        <Route path="/cart" element={<CartPage />} />
         <Route path="/overview/:productId" element={<ProductOverviewPage />} />
         <Route path="/*" element={<h1 className="text-center mt-10 text-3xl font-bold">404 - Page Not Found</h1>} />
       </Routes>
