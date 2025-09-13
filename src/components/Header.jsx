86aa32e37f89498eb091d8fe729130324f13bfd3
Chamod Malindu <chamod10malindu@gmail.com>
2025-09-13 10:01:01 +0530

    Change header background color from blue to accent 🪛

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { TiShoppingCart } from "react-icons/ti";
 
 export default function Header() {
   return(
-    <div className="w-full h-[100px] bg-blue-600 flex justify-center items-center text-white text-2xl mb-[20px] relative">
+    <div className="w-full h-[100px] bg-accent flex justify-center items-center text-white text-2xl mb-[20px] relative">
       <Link to="/">Home</Link>
       <Link to="/products" className="mx-5">Products</Link>
       <Link to="/reviews">Review</Link>
