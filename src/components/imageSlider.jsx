157423da9939535cb9b138a41245921ad6396235
Chamod Malindu <chamod10malindu@gmail.com>
2025-09-13 10:03:03 +0530

    Update ImageSlider component styles for responsiveness and layout adjustments 🪛

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -4,14 +4,14 @@ export default function ImageSlider({images}){ // const images = props.images;
   const[activeImageIndex, setActiveImageIndex] = useState(0);
 
   return(
-    <div className="w-[400px] h-[500px]">
-      <img src={images[activeImageIndex]} className="w-full h-[400px] object-cover" />
-      <div className="w-full h-[100px] flex flex-row items-center justify-center gap-1">
+    <div className="w-[300px] h-[400px] md:w-[400px] md:h-[500px]">
+      <img src={images[activeImageIndex]} className="w-full h-[320px] md:h-[400px] object-cover" />
+      <div className="w-full h-[50px] md:px-0 mt-[15px] md:mt-0 md:h-[100px] flex flex-row items-center justify-center gap-1">
         {
           images.map(
             (image, index)=> {
               return(
-                <img src={image} key={index} className={"w-[90px] h-[90px] object-cover cursor-pointer" + (activeImageIndex == index ? " border-[3px]" : "")} 
+                <img src={image} key={index} className={"w-[70px] h-[70px] md:w-[90px] md:h-[90px] object-cover cursor-pointer" + (activeImageIndex == index ? " border-[3px]" : "")} 
                 onClick={
                   ()=> {
                     setActiveImageIndex(index);
