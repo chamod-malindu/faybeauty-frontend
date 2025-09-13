6bd1b8f88feafde415e775d75806d2da77768ec6
Chamod Malindu <chamod10malindu@gmail.com>
2025-09-13 10:04:55 +0530

    Refactor ProductOverviewPage for mobile responsiveness and update button styles 👀

--- src/pages/client/productOverviewPage.jsx
+++ src/pages/client/productOverviewPage.jsx
@@ -30,17 +30,24 @@ export default function ProductOverviewPage() {
     }, [status]
     )
   return(
-    <div className="w-full h-screen">
+    <div className="w-full h-full bg-primary">
       {
         status == "loading" && <Loader />
       }
       {
-        status == "success" && <div className="w-full h-full flex flex-row"> 
-          <div className="w-[49%] h-full flex flex-col justify-center items-center">
+        status == "success" && <div className="w-full flex flex-col md:flex-row">
+          <div className="md:hidden px-[15px] flex justify-center items-center text-center mb-[20px]">
+              <h1 className="text-2xl font-bold">{product.name} <span className="font-light">{product.altNames.join(" | ")}</span>
+              </h1>
+            </div> 
+          <div className="w-[full] md:w-[49%] flex flex-col justify-center items-center">
             <ImageSlider images={product.images} /> 
           </div>
-          <div className="w-[49%] h-full flex flex-col justify-center items-center">
-            <h1 className="text-2xl font-bold">{product.name} <span className="font-light">{product.altNames.join(" | ")}</span></h1>
+          <div className="w-[full] md:w-[49%] h-full flex flex-col justify-center items-center px-[15px] md:px-0">
+            <div className="hidden md:block">
+              <h1 className="text-2xl font-bold">{product.name} <span className="font-light">{product.altNames.join(" | ")}</span>
+              </h1>
+            </div>
             <p className="text-lg mt-[20px]">{product.description}</p>
             <div className="w-full flex justify-center item-center mt-[20px]">
               {
@@ -56,7 +63,7 @@ export default function ProductOverviewPage() {
               }
             </div>
             <div className="w-full flex flex-row justify-center items-center mt-[20px]  gap-[10px]">
-              <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-900 border-[3px] border-blue-900 hover:bg-white hover:text-blue-900" onClick={
+              <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-accent border-[3px] border-accent hover:bg-white hover:text-accent" onClick={
                 () => {
                   navigate("/checkout", 
                     {
@@ -72,7 +79,7 @@ export default function ProductOverviewPage() {
                     });
                 }
               }>Buy Now</button>
-              <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-blue-600 border-[3px] border-blue-600 hover:bg-white hover:text-blue-600" onClick={
+              <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-accent-hover border-[3px] border-accent-hover hover:bg-white hover:text-accent-hover" onClick={
                 ()=> {
                   addToCart(product, 1);
                   toast.success("Product added to cart");
