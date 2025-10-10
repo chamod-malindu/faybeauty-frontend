9e8cb58179d8f88cd8976d309d88d472fa374b73
Chamod Malindu <chamod10malindu@gmail.com>
2025-10-11 01:58:08 +0530

    Add search functionality to products page 👀

--- src/pages/client/productsPage.jsx
+++ src/pages/client/productsPage.jsx
@@ -6,33 +6,56 @@ import ProduactCard from "../../components/productCard";
 export default function ProductsPage() {
   const[products, setProduct] = useState([]);
   const[isLoading, setIsLoading] = useState(true);
+  const[query, setQuery] = useState("");
 
   useEffect(
     () => {
       if(isLoading){
-        axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products").then(
-          (res)=> {
-            setProduct(res.data);
-            setIsLoading(false);
-        })  
-      }           
+        if(query == ""){
+          axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products").then(
+            (res)=> {
+              setProduct(res.data);
+              setIsLoading(false);
+          })  
+        }else{
+          axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products/search/"+query).then(
+            (res)=> {
+              setProduct(res.data);
+              setIsLoading(false);
+          })  
+        }    
+      }
     },
     [isLoading]
-  )
+)
   return (
   <div className="w-full min-h-screen bg-primary">
-    {
-      isLoading ? <Loader /> : 
-      <div className="w-full flex flex-wrap items-center justify-center gap-[30px] ">
-        {
-          products.map(
-            (product)=> {
-              return(
-                    <ProduactCard key={product.productId} product = {product} />   
-              )
-            })
+    <div className="w-full mb-[30px] mt-[30px] flex justify-center items-center">
+      <input type="text" placeholder="Search Products......." value={query} onChange={
+        (e) => {
+          setQuery(e.target.value);
+          setIsLoading(true);
         }
-      </div>
+      }
+      className="w-[400px] h-[40px] border-accent border-2 rounded-2xl p-2"></input>
+    </div>
+    {
+      isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full flex flex-wrap items-center justify-center gap-[30px]">
+          {products.length > 0 ? (
+            products.map((product) => (
+              <ProduactCard key={product.productId} product={product} />
+            ))
+          ) : (
+            <p className="text-accent text-lg mt-6">
+              No products found for "{query}".
+            </p>
+          )}
+        </div>
+      )
+      
     }
   </div>
   );
