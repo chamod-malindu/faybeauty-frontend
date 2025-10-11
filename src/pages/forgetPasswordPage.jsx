6361c76a18aaaf8d3c701b0d9983aacf35a714d7
Chamod Malindu <chamod10malindu@gmail.com>
2025-10-11 17:38:55 +0530

    Implement OTP functionality in Forget Passwordpage 💥

--- src/pages/forgetPasswordPage.jsx
+++ src/pages/forgetPasswordPage.jsx
@@ -1,45 +1,108 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 
 export default function ForgetPasswordPage() {
+  const[emailSent, setEmailSent] = useState(false);
+  const [email, setEmail] = useState("")
+
+  async function sendOTP(){
+    try{
+      await axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/send-otp", { email: email });
+      toast.success("OTP sent to your email");
+      setEmailSent(true);
+
+    }catch(error){
+      console.log(error);
+      toast.error("Failed to send OTP");
+    }
+  }
+
   return (
     <div className="w-full min-h-screen flex justify-center items-center bg-primary">
-      <div className="w-[500px] h-[400px] bg-white shadow-2xl rounded-[30px] flex flex-col justify-center items-center relative gap-[12px] p-6">
+      {
+        !emailSent ? (
+          <div className="w-[500px] h-[400px] bg-white shadow-2xl rounded-[30px] flex flex-col justify-center items-center relative gap-[12px] p-6">
         
-        {/* Heading */}
-        <h1 className="absolute top-[65px] text-2xl font-semibold text-secondary flex flex-col justify-center items-center">
-          Reset Password
-        </h1>
-        
-        {/* Subtext */}
-        <div className="flex flex-col items-center text-center mt-16 gap-1">
-          <h2 className="text-xl text-secondary font-medium">
-            Enter Your Email Address
-          </h2>
-          <span className="text-secondary text-sm">
-            You will receive an OTP code
-          </span>
+          {/* Heading */}
+          <h1 className="absolute top-[65px] text-2xl font-semibold text-secondary flex flex-col justify-center items-center">
+            Reset Password
+          </h1>
+          
+          {/* Subtext */}
+          <div className="flex flex-col items-center text-center mt-16 gap-1">
+            <h2 className="text-xl text-secondary font-medium">
+              Enter Your Email Address
+            </h2>
+            <span className="text-secondary text-sm">
+              You will receive an OTP code
+            </span>
+          </div>
+          
+          {/* Input */}
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            className="w-[300px] h-[40px] border-2 border-accent rounded-xl px-3 focus:outline-none focus:border-accent-hover focus:ring-2 focus:ring-accent-hover transition"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          
+          {/* Button */}
+          <button className="w-[300px] h-[40px] bg-accent text-white rounded-xl mt-2 hover:bg-accent-hover transition" onClick={sendOTP}>
+            Send OTP
+          </button>
+          
+          {/* Back link */}
+          <Link
+            to="/login"
+            className="text-accent hover:underline text-sm mt-2"
+          >
+            Back to Login page
+          </Link>
         </div>
+        ) : (
+          <div className="w-[500px] h-[400px] bg-white shadow-2xl rounded-[30px] flex flex-col justify-center items-center relative gap-[12px] p-6">
         
-        {/* Input */}
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="w-[300px] h-[40px] border-2 border-accent rounded-xl px-3 focus:outline-none focus:border-accent-hover focus:ring-2 focus:ring-accent-hover transition"
-        />
-        
-        {/* Button */}
-        <button className="w-[300px] h-[40px] bg-accent text-white rounded-xl mt-2 hover:bg-accent-hover transition">
-          Send OTP
-        </button>
-        
-        {/* Back link */}
-        <Link
-          to="/login"
-          className="text-accent hover:underline text-sm mt-2"
-        >
-          Back to Login page
-        </Link>
-      </div>
+            {/* Heading */}
+            <h1 className="absolute top-[65px] text-2xl font-semibold text-secondary flex flex-col justify-center items-center">
+              OTP Verification
+            </h1>
+            
+            {/* Subtext */}
+            <div className="flex flex-col items-center text-center mt-16 gap-1">
+              <h2 className="text-xl text-secondary font-medium">
+                Enter Your OTP
+              </h2>
+              <span className="text-secondary text-sm">
+                We have sent an OTP to your email
+              </span>
+            </div>
+            
+            {/* Input */}
+            <input
+              type="number"
+              placeholder="Enter OTP"
+              className="w-[300px] h-[40px] border-2 border-accent rounded-xl px-3 focus:outline-none focus:border-accent-hover focus:ring-2 focus:ring-accent-hover transition"
+            />
+            
+            {/* Button */}
+            <button className="w-[300px] h-[40px] bg-accent text-white rounded-xl mt-2 hover:bg-accent-hover transition">
+              Verify OTP
+            </button>
+            
+            {/* Back link */}
+            <Link
+              to="/login"
+              className="text-accent hover:underline text-sm mt-2"
+            >
+              Back to Login page
+            </Link>
+          </div>  
+        )
+      }
+      
     </div>
   );
 }
