d74ff64247445cb3eb4c03dc8a864ab3076fb5a3
Chamod Malindu <chamod10malindu@gmail.com>
2025-10-11 10:27:17 +0530

    Refactor loginPage component 🪛 - enhance styling, add placeholders, and set forget pasword option

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -37,34 +37,59 @@ export default function loginPage() {
 
   return (
     <div className="w-full h-screen bg-[url(./loginbg.jpg)] bg-cover bg-center flex">
-      <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] mt-[9%]  ml-[9%] relative flex flex-col items-center justify-center gap-[20px] ">
-        <h1 className="absolute top-[20px] text-2xl font-bold text-center my-5">Login</h1>
+      <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] mt-[7%] ml-[9%] relative flex flex-col items-center justify-center gap-[20px]">
+        
+        {/* Heading */}
+        <h1 className="absolute top-[20px] text-2xl font-bold text-accent-hover text-center my-5">
+          Login
+        </h1>
+
+        {/* Email Input field */}
         <div className="w-[350px] flex flex-col">
-          <span className="text-lg">Email</span>
-          <input type="text" className="w-[350px] h-[40px] border rounded-xl" 
-          onChange={
-            (e) => {
-              setEmail(e.target.value);
-            }
-          }/>
+          <span className="text-lg text-accent-hover mb-[2px] font-semibold">Email</span>
+          <input
+            type="text"
+            className="w-[350px] h-[40px] text-accent-hover pl-[10px] border rounded-xl border-accent focus:text-white focus:pl-[10px]"
+            placeholder="eg: example@email.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div className="flex flex-col"> 
-          <span className="text-lg">Password</span>
-          <input type="password" className="w-[350px] h-[40px] border rounded-xl" 
-          onChange={
-            (e) => {
-              setPassword(e.target.value);
-            }
-          }/>
+
+        {/* Password Input field */}
+        <div className="flex flex-col">
+          <span className="text-lg text-accent-hover mb-[2px] font-semibold">Password</span>
+          <input
+            type="password"
+            className="w-[350px] h-[40px] border border-accent rounded-xl focus:text-white focus:pl-[10px]"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button className="w-[350px] h-[40px] bg-amber-900 rounded-xl text-lg mt-5 hover:bg-amber-950 transition-all duration-300"
-        onClick={login}>
+
+        {/* Forget Password Link */}
+        <Link
+          to="/forget-password"
+          className="absolute left-[77px] top-[280px] text-accent text-[14px] hover:text-white"
+        >
+          Forget Password?
+        </Link>
+
+        {/* Login Button */}
+        <button
+          className="w-[350px] h-[40px] bg-accent-hover rounded-xl mt-5 text-lg text-white hover:bg-accent hover:border-accent hover:border cursor-pointer transition-all duration-300"
+          onClick={login}
+        >
           Login Now
         </button>
-        <p>Don't have an account? <Link to="/register" children>Sign up</Link> from here</p>
 
+        {/* Sign Up Link */}
+        <p className="text-white text-[14px]">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-accent hover:text-white">
+            Sign up
+          </Link>{" "}
+          from here
+        </p>
       </div>
-
     </div>
-  )
+  );
 }
