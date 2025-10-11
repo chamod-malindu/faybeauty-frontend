f5cadea97c903fc120b6c76794924d7c7f4766f1
Chamod Malindu <chamod10malindu@gmail.com>
2025-10-11 10:28:03 +0530

    Create Forget Password page 👀

---
+++ src/pages/forgetPasswordPage.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+
+export default function ForgetPasswordPage() {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center bg-primary">
+      <div className="w-[500px] h-[400px] bg-white shadow-2xl rounded-[30px] flex flex-col justify-center items-center relative gap-[12px] p-6">
+        
+        {/* Heading */}
+        <h1 className="absolute top-[65px] text-2xl font-semibold text-secondary flex flex-col justify-center items-center">
+          Reset Password
+        </h1>
+        
+        {/* Subtext */}
+        <div className="flex flex-col items-center text-center mt-16 gap-1">
+          <h2 className="text-xl text-secondary font-medium">
+            Enter Your Email Address
+          </h2>
+          <span className="text-secondary text-sm">
+            You will receive an OTP code
+          </span>
+        </div>
+        
+        {/* Input */}
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="w-[300px] h-[40px] border-2 border-accent rounded-xl px-3 focus:outline-none focus:border-accent-hover focus:ring-2 focus:ring-accent-hover transition"
+        />
+        
+        {/* Button */}
+        <button className="w-[300px] h-[40px] bg-accent text-white rounded-xl mt-2 hover:bg-accent-hover transition">
+          Send OTP
+        </button>
+        
+        {/* Back link */}
+        <Link
+          to="/login"
+          className="text-accent hover:underline text-sm mt-2"
+        >
+          Back to Login page
+        </Link>
+      </div>
+    </div>
+  );
+}
