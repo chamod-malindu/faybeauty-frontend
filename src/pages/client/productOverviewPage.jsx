269d7ca0bd0ab47c4e1643d995e8ee247a9ff971
Chamod Malindu <chamod10malindu@gmail.com>
2025-11-16 00:25:26 +0530

    Fetch and display product reviews in ProductOverviewPage component 🪛

--- src/pages/client/productOverviewPage.jsx
+++ src/pages/client/productOverviewPage.jsx
@@ -5,19 +5,22 @@ import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../../components/loader";
 import ImageSlider from "../../components/imageSlider";
 import { addToCart, getCart } from "../../utils/cart";
-import { IoCheckmarkDone } from "react-icons/io5";
+import { ProductReview } from "../../components/review/productReviews";
 
 export default function ProductOverviewPage() {
   const navigate = useNavigate();
   const params = useParams();
   const[product, setProduct] = useState(null);
   const[status, setStatus] = useState("loading"); // loading, success, error
+  const[reviews, setReviews] = useState([]);
+
+  const productId = params.productId;
 
   useEffect(
     () => {
 
       if(status == "loading") {
-        axios.get(import.meta.env.VITE_BACKEND_URL+`/api/products/${params.productId}`,
+        axios.get(import.meta.env.VITE_BACKEND_URL+`/api/products/${productId}`,
         ).then(
           (res) => {
             setProduct(res.data.product);
@@ -31,7 +34,25 @@ export default function ProductOverviewPage() {
         )
       }
     }, [status]
-    )
+  )
+
+  useEffect(
+    () => {
+      axios.get(import.meta.env.VITE_BACKEND_URL+`/api/reviews/${productId}`,
+      ).then(
+        (res) => {
+          console.log(res.data.reviews);
+          setReviews(res.data.reviews);
+        }
+      ).catch(
+        (err) => {
+          console.log("Failed to fetch reviews.");
+        }
+      )
+
+    }, [params.productId]
+  )
+
   return(
     <div className="w-full min-h-screen pb-[50px] bg-primary">
       {
@@ -50,7 +71,7 @@ export default function ProductOverviewPage() {
               </div>
               <div className="w-[full] md:w-[49%] h-full flex flex-col justify-center items-center px-[15px] md:px-0">
               <div className="hidden md:block">
-                <h1 className="text-2xl font-bold">{product.name} <span className="font-light">{product.altNames.join(" | ")}</span>
+                <h1 className="text-3xl font-bold flex flex-col justify-center items-center">{product.name} <span className="font-light text-xl">{product.altNames.join(" | ")}</span>
                 </h1>
               </div>
               <p className="text-lg mt-[20px]">{product.description}</p>
@@ -58,8 +79,8 @@ export default function ProductOverviewPage() {
                 {
                   product.labelledPrice > product.price?
                   <div>
-                  <span className="text-2xl font-semibold  line-through mr-[20px]">{product.labelledPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span> 
-                  <span className="text-3xl font-bold ">{product.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                  <span className="text-3xl font-bold mr-[20px]">Rs.{product.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                  <span className="text-2xl font-semibold  line-through">Rs.{product.labelledPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span> 
                   </div>
                   :
                   <div>
@@ -67,6 +88,7 @@ export default function ProductOverviewPage() {
                   </div>
                 }
               </div>
+
               <div className="w-full flex flex-row justify-center items-center mt-[20px]  gap-[10px] absolute bottom-[160px]">
                 <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white bg-accent border-[3px] border-accent hover:bg-white hover:text-accent" onClick={
                   () => {
@@ -99,27 +121,11 @@ export default function ProductOverviewPage() {
               </div>
             </div>
           </div>
+
           <div className="mb-6">
             <h1 className="text-3xl font-bold text-secondary my-[40px]">Customer Reviews</h1>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="bg-white rounded-xl shadow-md p-6 hover:shadow-lg transition-shadow">
-              <div className="flex flex-row">
-                <h2 className="font-semibold text-lg text-secondary">Nimedi</h2>
-                <span className="text-sm text-green-800 ml-1 px-2 py-1 bg-green-300 rounded-xl"><IoCheckmarkDone className="inline-block mr-1" />Verified Purchase</span>
-              </div>              
-              <p className="text-yellow-400 my-1 text-2xl">☆☆☆☆☆</p>
-              <p className="text-secondary/70 leading-relaxed">Review content goes here...</p>
-            </div>
-            <div className="bg-white rounded-xl shadow-md p-6 hover:shadow-lg transition-shadow">
-              <div className="flex flex-row">
-                <h2 className="font-semibold text-lg text-secondary">Nimedi</h2>
-                <span className="text-sm text-green-800 ml-1 px-2 py-1 bg-green-300 rounded-xl"><IoCheckmarkDone className="inline-block mr-1" />Verified Purchase</span>
-              </div> 
-              <p className="text-yellow-400 my-1 text-2xl">☆☆☆☆☆</p>
-              <p className="text-secondary/70 leading-relaxed">Review content goes here...</p>
-            </div>
-          </div>
+          <ProductReview reviews={reviews} />
         </div>
       )
       }
