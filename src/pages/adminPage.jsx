93782d2449337ba0d4157360b580d60e13e45413
Chamod Malindu <chamod10malindu@gmail.com>
2025-11-25 17:40:56 +0530

    Enhance admin page layout and add user management route 👀

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -7,26 +7,28 @@ import ProductsAdminPage from "./admin/productsAdminPage";
 import AddProductAdminPage from "./admin/addProductAdminPage";
 import UpdateProductPage from "./admin/updateProductPage";
 import OrdersAdminPage from "./admin/ordersAdminPage";
+import UserManagementPage from "./admin/userManagementPage";
 
 export default function adminPage() {
 
   return(
-    <div className="w-full h-screen  flex">
-      <div className="w-[300px] h-screen flex flex-col items-center">
+    <div className="w-full h-screen flex">
+      <div className="w-[300px] h-screen flex flex-col items-center shadow-xl">
         <span className="text-3xl font-bold my-5"> Admin Panel </span>
-        <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/products"> <AiFillProduct /> Products </Link>
-        <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/orders"> <HiShoppingBag /> Orders </Link>
-        <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/users"> <ImUsers /> Users </Link>
-        <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/settings"> <IoSettings /> Settings </Link>
+        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/products"> <AiFillProduct /> Products </Link>
+        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/orders"> <HiShoppingBag /> Orders </Link>
+        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/users"> <ImUsers /> Users </Link>
+        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/settings"> <IoSettings /> Settings </Link>
 
       </div>
-      <div className="w-[calc(100%-300px)] h-screen">
+      <div className="w-[calc(100%-300px)] h-screen px-10 py-7">
         <Routes path="/">
           <Route path="/" element={<h1>Dashboard</h1>}/>
           <Route path="/products" element={<ProductsAdminPage />}/>
           <Route path="/newProduct" element={<AddProductAdminPage />}/>
           <Route path="/orders" element={<OrdersAdminPage />}/>
           <Route path="/updateProduct" element={<UpdateProductPage />}/>
+          <Route path="/users" element={<UserManagementPage />}/>
         </Routes>
         </div>
     </div>
