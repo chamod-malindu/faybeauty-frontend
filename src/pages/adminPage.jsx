2240e04d8bce6252e7a4c62ae71c6cec0d9f88b5
Chamod Malindu <chamod10malindu@gmail.com>
2025-12-03 19:56:56 +0530

    Display username and add logout button in  admin page layout for improved styling and consistency 🚀

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -15,17 +15,26 @@ import DashboardAdminPage from "./admin/dashboardAdminPage";
 export default function adminPage() {
 
   return(
-    <div className="w-full h-full flex">
-      <div className="w-[300px] min-h-screen flex flex-col items-center shadow-xl">
-        <span className="text-3xl font-bold my-5"> Admin Panel </span>
-        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin"> <MdDashboard /> Dashboard </Link>
-        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/products"> <GiLipstick /> Products </Link>
-        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/orders"> <HiShoppingBag /> Orders </Link>
-        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/users"> <ImUsers /> Users </Link>
-        <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/settings"> <IoSettings /> Settings </Link>
-
+    <div className="w-screen h-full flex m-0 p-0">
+      <div className="w-[280px] min-h-screen flex flex-col items-center shadow-xl">
+          <span className="text-3xl font-bold my-5"> Admin Panel </span>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin"> <MdDashboard /> Dashboard </Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/products"> <GiLipstick /> Products </Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/orders"> <HiShoppingBag /> Orders </Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/users"> <ImUsers /> Users </Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center text-xl gap-[25px] hover:bg-accent hover:text-white" to="/admin/settings"> <IoSettings /> Settings </Link>
       </div>
-      <div className="w-[calc(100%-300px)] h-full px-10 py-7">
+      <div className="w-[calc(100%-300px)] h-full pl-10 pr-5 pt-3 pb-7">
+        <div className="h-15 flex justify-end items-center mb-2">
+          <div className="flex items-center gap-2">
+            <div className="w-10 h-10 bg-amber-300 rounded-full">
+            </div>
+            <h2 className="text-lg font-mono">Kamal Perera</h2>
+            <button className="bg-accent py-2 px-3 font-mono font-semibold rounded-2xl ml-3 hover:bg-accent-hover items-center justify-center shadow-lg border-2 text-lg border-accent cursor-pointer" >
+            Logout
+          </button>
+          </div>
+        </div>
         <Routes path="/">
           <Route path="/" element={<DashboardAdminPage />}/>
           <Route path="/products" element={<ProductsAdminPage />}/>
