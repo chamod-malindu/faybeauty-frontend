bdbbe2961a3e8775254b57f806ffff469f6de793
Chamod Malindu <chamod10malindu@gmail.com>
2025-12-12 21:22:33 +0530

    Fetch and display dashboard overview data with error handling 🚀 - Desplay Total Revenue, Total Orders, Active Users, Total Products - implement weekly revenue area chart and monthly user registration bar chart

--- src/pages/admin/dashboardAdminPage.jsx
+++ src/pages/admin/dashboardAdminPage.jsx
@@ -5,48 +5,41 @@ import { FaUserFriends } from "react-icons/fa";
 import { SiDropbox } from "react-icons/si";
 import { AreaChart,BarChart, Bar, Rectangle, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 import TitleHeaderDashboard from "../../components/TitleHeader";
+import { useEffect, useState } from "react";
+import fetchDashboardOverview from "../../services/dashboardService";
 
-export const SALES_DATA = [
-  { name: 'Mon', revenue: 4000, orders: 24 },
-  { name: 'Tue', revenue: 3000, orders: 18 },
-  { name: 'Wed', revenue: 2000, orders: 12 },
-  { name: 'Thu', revenue: 2780, orders: 20 },
-  { name: 'Fri', revenue: 1890, orders: 15 },
-  { name: 'Sat', revenue: 2390, orders: 22 },
-  { name: 'Sun', revenue: 3490, orders: 30 },
-];
 
-const userData = [
-  { month: "Jan", users: 25 },
-  { month: "Feb", users: 40 },
-  { month: "Mar", users: 18 },
-  { month: "Apr", users: 30 },
-  { month: "May", users: 22 },
-  { month: "Jun", users: 41 },
-  { month: "Jul", users: 55 },
-  { month: "Aug", users: 33 },
-  { month: "Sep", users: 27 },
-  { month: "Oct", users: 15 },
-  { month: "Nov", users: 12 },
-  { month: "Dec", users: 20 },
-];
 
 export default function DashboardAdminPage() {
+  const [overview, setOverview] = useState(null);
+
+  useEffect(() => {
+    const dashboardData =  async () => {
+      try {
+        const response = await fetchDashboardOverview();
+        setOverview(response);
+        console.log("Dashboard overview data:", response);
+      } catch (error) {
+        console.error("Error fetching dashboard overview:", error);
+      }
+    } 
+    dashboardData();
+  }, [])
   return (
     <div className="w-full h-full">
       
       <TitleHeaderDashboard title="Dashboard Overview" subtitle="Welcome back to Faye Beauty Admin." />
 
       <div className="grid grid-cols-4 gap-2 w-full mb-6">
-        <Card title="Total Revenue" amount="$0.00" percentage="12.5%" icon={<MdOutlineAttachMoney size={20} />} />
-        <Card title="Total Orders" amount="$0.00" percentage="12.5%" icon={<TiShoppingCart size={20} />} />
-        <Card title="Active Users" amount="$0.00" percentage="12.5%" icon={<FaUserFriends size={20} />} />
-        <Card title="Total Products" amount="$0.00" percentage="12.5%" icon={<SiDropbox size={20} />} />
+        <Card title="Total Revenue" amount={`${overview ? `$${overview.totalRevanue}` : "$0.00"}`} percentage="12.5%" icon={<MdOutlineAttachMoney size={20} />} />
+        <Card title="Total Orders" amount={`${overview ? overview.totalOrders : "0"}`} percentage="12.5%" icon={<TiShoppingCart size={20} />} />
+        <Card title="Active Users" amount={`${overview ? overview.totalUsers : "0"}`} percentage="12.5%" icon={<FaUserFriends size={20} />} />
+        <Card title="Total Products" amount={`${overview ? overview.totalProducts : "0"}`} percentage="12.5%" icon={<SiDropbox size={20} />} />
       </div>
       <div className="grid grid-cols-2 gap-2">
         <div className=" bg-white rounded-2xl py-5 pr-5">
           <ResponsiveContainer width="100%" height="100%">
-            <AreaChart data={SALES_DATA}>
+            <AreaChart data={overview?.salesData || []}>
               <defs>
                 <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor="#c28d59" stopOpacity={0.2}/>
@@ -68,7 +61,7 @@ export default function DashboardAdminPage() {
           <BarChart
             style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
             responsive
-            data={userData}
+            data={overview?.monthlyUsers || []}
             margin={{
               top: 5,
               right: 0,
@@ -81,7 +74,7 @@ export default function DashboardAdminPage() {
             <YAxis width="auto" />
             <Tooltip />
             <Legend />
-            <Bar dataKey="users" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+            <Bar dataKey="users" fill="#c28d59" activeBar={<Rectangle fill="#dba672" stroke="#c28d59" />} />
           </BarChart>
         </div>
       </div>
