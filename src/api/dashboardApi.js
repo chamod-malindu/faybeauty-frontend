10ba24c48ce04bb8abbf17a566a6753f724493ff
Chamod Malindu <chamod10malindu@gmail.com>
2025-12-24 22:50:55 +0530

    Add API end point for fetch client dashboard overview data 👀

--- src/api/dashboardApi.js
+++ src/api/dashboardApi.js
@@ -1,12 +1,22 @@
 import api from "../config/axiosConfig";
 
 
-export default async function fetchDashboardData() {
+export async function fetchDashboardData() {
   try {
     const response = await api.get("/dashboard");
     return response.data;
   } catch (error) {
     console.error("Failed to fetch dashboard data", error);
+    throw error;
+  }
+}
+
+export async function fetchClientDashboardData() {
+  try {
+    const response = await api.get("/dashboard/client");
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch client dashboard data", error);
     throw error;
   }
 }
