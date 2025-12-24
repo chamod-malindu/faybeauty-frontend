5f489ba19cec08e72372c58e9ff24e31170c6f13
Chamod Malindu <chamod10malindu@gmail.com>
2025-12-24 22:52:42 +0530

    Add fetchClientDashboardOverview function to retrieve client dashboard data 👀

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,8 +1,8 @@
 import toast from "react-hot-toast";
-import fetchDashboardData from "../api/dashboardApi";
+import  { fetchDashboardData, fetchClientDashboardData } from "../api/dashboardApi";
 
 
-export default async function fetchDashboardOverview() {
+export async function fetchDashboardOverview() {
   try {
     const response = await fetchDashboardData();
     console.log("Dashboard data fetched:", response);
@@ -10,6 +10,18 @@ export default async function fetchDashboardOverview() {
   } catch (error) {
     toast.error("Failed to fetch dashboard data");
     console.error("Error fetching dashboard data:", error);
+    throw error;
+  }
+}
+
+
+export async function fetchClientDashboardOverview() {
+  try {
+    const response = await fetchClientDashboardData();
+    return response;
+  } catch (error) {
+    toast.error("Failed to fetch client dashboard data");
+    console.error("Error fetching client dashboard data:", error);
     throw error;
   }
 }
