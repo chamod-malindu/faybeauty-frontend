30c5ffc7c50e7e478174c678646526880ef69835
Chamod Malindu <chamod10malindu@gmail.com>
2025-12-30 23:55:18 +0530

    Update ClientHomePage to use HomePage component and add Footer for improved layout 👀

--- src/pages/client/clientHomePage.jsx
+++ src/pages/client/clientHomePage.jsx
@@ -8,6 +8,8 @@ import OrdersHistoryPage from "./ordersHistoryPage";
 import ContactUs from "./contactPage";
 import AboutUs from "./aboutUsPage";
 import SiteReviews from "./siteReviewPage";
+import HomePage from "./homePage";
+import Footer from "../../components/Footer";
 
 
 export default function ClientHomePage() {
@@ -15,17 +17,18 @@ export default function ClientHomePage() {
     <div className="w-full h-screen">
       <Header />
       <Routes path="/">
-        <Route path="/" element={<h1 className="text-center mt-10 text-3xl font-bold">Welcome to the Home Page</h1>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/reviews" element={<SiteReviews />} />
-        <Route path="/aboutUS" element={<AboutUs />} />
-        <Route path="/contactUS" element={<ContactUs />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/contactUs" element={<ContactUs />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/overview/:productId" element={<ProductOverviewPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/orders" element={<OrdersHistoryPage />} />
         <Route path="/*" element={<h1 className="text-center mt-10 text-3xl font-bold">404 - Page Not Found</h1>} />
       </Routes>
+      <Footer />
     </div>
   )
 
